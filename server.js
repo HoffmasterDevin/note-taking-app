f1930baca55794b41cde1212e1f0fd3c48546bdb
HoffmasterDevin <118146567+HoffmasterDevin@users.noreply.github.com>
2023-05-02 11:20:25 -0500

    added get/post apiRoutes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const htmlRoutes = require("./routes/htmlRoutes");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-app.use(apiRoutes);
+//app.use(apiRoutes);
 app.use(htmlRoutes);
 
 app.listen(PORT, () => {
