9fdf732f956bde44098ed0448adc7aa4ec74bf7f
HoffmasterDevin <118146567+HoffmasterDevin@users.noreply.github.com>
2023-05-02 12:10:38 -0500

    added helpers

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path');
 const fs = require('fs');
 const uuid = require('./helpers/uuid');
-const { readAndAppend, readFromFile, writeToFile } = require('./helpers/fsUtils');
+const { readAndAppend, readFromFile, writeToFile } = require('./helpers/utils');
 
 const app = express()
 const PORT = process.env.PORT || 3001
